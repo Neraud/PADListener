8fde283d1b72d16ac2b634869b8af1bb60b1223b
neraud <neraud.login@gmail.com>
2014-06-03 23:35:21 +0200

    Bumped version to 0.9.2

--- PADListener/src/fr/neraud/padlistener/constant/PadListenerVersion.java
+++ PADListener/src/fr/neraud/padlistener/constant/PadListenerVersion.java
@@ -8,6 +8,6 @@ package fr.neraud.padlistener.constant;
  */
 public interface PadListenerVersion {
 
-	public static final String PAD_LISTENER_VERSION = "0.9.1";
+	public static final String PAD_LISTENER_VERSION = "0.9.2";
 
 }
