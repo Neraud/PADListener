49061551d9ec09a629ea4624a6d933a101bb608f
neraud <neraud.login@gmail.com>
2014-06-07 14:43:30 +0200

    Added helper to load mock captured data

---
+++ PADListener/src/fr/neraud/padlistener/helper/MockCaptureHelper.java
@@ -0,0 +1,66 @@
+
+package fr.neraud.padlistener.helper;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.List;
+
+import org.apache.commons.io.IOUtils;
+
+import android.content.Context;
+import android.util.Log;
+import fr.neraud.padlistener.pad.constant.ApiAction;
+import fr.neraud.padlistener.pad.model.ApiCallModel;
+import fr.neraud.padlistener.proxy.plugin.ApiCallHandlerThread;
+
+/**
+ * Helper to load a mock capture
+ * 
+ * @author Neraud
+ */
+public class MockCaptureHelper {
+
+	private static final String MOCK_CAPTURE_FILE_NAME = "capture.json";
+	private final Context context;
+
+	public MockCaptureHelper(Context context) {
+		super();
+		this.context = context;
+	}
+
+	public void loadMockCapture() {
+		Log.d(getClass().getName(), "loadMockCapture");
+		try {
+			final ApiCallModel model = new ApiCallModel();
+			model.setAction(ApiAction.GET_PLAYER_DATA);
+			//model.setRequestParams(requestParams);
+			//model.setRequestContent(requestContentString);
+			model.setResponseContent(extractResponseContent());
+			new ApiCallHandlerThread(context, model).start();
+		} catch (final IOException e) {
+			Log.e(getClass().getName(), "loadMockCapture", e);
+		}
+	}
+
+	private String extractResponseContent() throws IOException {
+		InputStream is = null;
+		String result = null;
+		try {
+			is = context.getAssets().open(MOCK_CAPTURE_FILE_NAME);
+			final List<String> lines = IOUtils.readLines(is);
+			final StringBuilder builder = new StringBuilder();
+			for (final String line : lines) {
+				builder.append(line);
+			}
+			result = builder.toString();
+		} finally {
+			if (is != null) {
+				try {
+					is.close();
+				} catch (final IOException e) {
+				}
+			}
+		}
+		return result;
+	}
+}
