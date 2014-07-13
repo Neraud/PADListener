543d8f58cab471a0d76a5361c08ae28761e2171a
neraud <neraud.login@gmail.com>
2014-07-13 17:29:14 +0200

    Improved SwitchListener (backgound task, layout)

---
+++ PADListenerApp/src/main/java/fr/neraud/padlistener/service/task/StopListenerAsyncTask.java
@@ -0,0 +1,63 @@
+package fr.neraud.padlistener.service.task;
+
+import android.content.Context;
+import android.os.AsyncTask;
+import android.util.Log;
+
+import fr.neraud.padlistener.constant.ProxyMode;
+import fr.neraud.padlistener.helper.TechnicalSharedPreferencesHelper;
+import fr.neraud.padlistener.proxy.helper.IptablesHelper;
+import fr.neraud.padlistener.proxy.helper.ProxyHelper;
+import fr.neraud.padlistener.proxy.helper.WifiAutoProxyHelper;
+import fr.neraud.padlistener.service.task.model.SwitchListenerResult;
+
+/**
+ * AsyncTask used to stop the Listener
+ *
+ * Created by Neraud on 13/07/2014.
+ */
+public class StopListenerAsyncTask extends AsyncTask<Void, Void, SwitchListenerResult> {
+
+	private final Context context;
+	private final ProxyHelper proxyHelper;
+
+	public StopListenerAsyncTask(Context context, ProxyHelper proxyHelper) {
+		this.context = context;
+		this.proxyHelper = proxyHelper;
+	}
+
+	@Override
+	protected SwitchListenerResult doInBackground(Void... params) {
+		Log.d(getClass().getName(), "doInBackground");
+		final SwitchListenerResult result = new SwitchListenerResult();
+
+		try {
+			proxyHelper.deactivateProxy();
+
+			final TechnicalSharedPreferencesHelper techPrefHelper = new TechnicalSharedPreferencesHelper(context);
+			final ProxyMode proxyMode = techPrefHelper.getLastListenerStartProxyMode();
+			switch (proxyMode) {
+				case AUTO_IPTABLES:
+					final IptablesHelper iptablesHelper = new IptablesHelper(context);
+					iptablesHelper.deactivateIptables();
+					break;
+				case AUTO_WIFI_PROXY:
+					final WifiAutoProxyHelper wifiAutoProxyHelper = new WifiAutoProxyHelper(context);
+					wifiAutoProxyHelper.deactivateAutoProxy();
+					break;
+				case MANUAL:
+				default:
+					// Nothing to do
+					break;
+			}
+
+			result.setSuccess(true);
+		} catch (final Exception e) {
+			Log.e(getClass().getName(), "PADListener stop failed  : " + e.getMessage());
+			result.setSuccess(false);
+			result.setError(e);
+		}
+
+		return result;
+	}
+}
