e4a4787e88fd2a4bea03e0a6acfc6b3a2096040b
neraud <neraud.login@gmail.com>
2014-09-29 21:09:32 +0200

    Reworked UI/UX : Manage Ignore List : use cards for list

--- PADListenerApp/src/main/java/fr/neraud/padlistener/ui/model/MonsterIgnoredCard.java
+++ PADListenerApp/src/main/java/fr/neraud/padlistener/ui/model/MonsterIgnoredCard.java
@@ -1,6 +1,8 @@
 package fr.neraud.padlistener.ui.model;
 
 import android.content.Context;
+import android.graphics.Color;
+import android.graphics.PorterDuff;
 import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
@@ -19,7 +21,7 @@ public class MonsterIgnoredCard extends AbstractMonsterCard {
 	private ManageIgnoreListTaskFragment mTaskFragment;
 
 	public MonsterIgnoredCard(Context context, MonsterInfoModel model, ManageIgnoreListTaskFragment taskFragment) {
-		super(context, model, R.layout.card_monster_info);
+		super(context, model, R.layout.card_monster_ignored);
 		mModel = model;
 		mTaskFragment = taskFragment;
 	}
@@ -29,11 +31,29 @@ public class MonsterIgnoredCard extends AbstractMonsterCard {
 		if (view == null) return;
 
 		final ImageView monsterImageView = fillImage(view);
+		monsterImageView.clearColorFilter();
+
+		final ImageView removeImageView = (ImageView) view.findViewById(R.id.card_monster_ignored_remove_image);
+		removeImageView.setVisibility(View.GONE);
 
 		monsterImageView.setOnClickListener(new View.OnClickListener() {
 			@Override
 			public void onClick(View view) {
 				Log.d(getClass().getName(), "monsterImageView.onClick :" + mModel.getIdJP());
+				if(removeImageView.getVisibility() == View.GONE) {
+					removeImageView.setVisibility(View.VISIBLE);
+					monsterImageView.setColorFilter(Color.parseColor("#99000000"), PorterDuff.Mode.DARKEN);
+				} else {
+					removeImageView.setVisibility(View.GONE);
+					monsterImageView.clearColorFilter();
+				}
+			}
+		});
+
+		removeImageView.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View view) {
+				Log.d(getClass().getName(), "removeImageView.onClick :" + mModel.getIdJP());
 				mTaskFragment.removeIgnoredIds(mModel.getIdJP());
 			}
 		});
