ad409380999d96d72d7eab5237ec3e5bf6ae27cf
neraud <neraud.login@gmail.com>
2014-10-05 15:48:18 +0200

    Reworked UI/UX : added view captured data help

--- PADListenerApp/src/main/java/fr/neraud/padlistener/ui/activity/ViewCapturedDataActivity.java
+++ PADListenerApp/src/main/java/fr/neraud/padlistener/ui/activity/ViewCapturedDataActivity.java
@@ -3,6 +3,7 @@ package fr.neraud.padlistener.ui.activity;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.support.v4.view.ViewPager;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -10,6 +11,8 @@ import android.view.MenuItem;
 import fr.neraud.padlistener.R;
 import fr.neraud.padlistener.helper.JsonCaptureHelper;
 import fr.neraud.padlistener.ui.constant.NavigationDrawerItem;
+import fr.neraud.padlistener.ui.helper.BaseHelpManager;
+import fr.neraud.padlistener.ui.model.ShowcaseHelpPageModel;
 
 /**
  * Activity to view captured data
@@ -39,7 +42,7 @@ public class ViewCapturedDataActivity extends AbstractPADListenerActivity {
 	@Override
 	public boolean onOptionsItemSelected(MenuItem item) {
 		switch (item.getItemId()) {
-			case R.id.view_captured_data_action_share:
+			case R.id.menu_view_captured_data_action_share:
 				final JsonCaptureHelper jsonCaptureHelper = new JsonCaptureHelper(this);
 				if (jsonCaptureHelper.hasPadCapturedData()) {
 					Intent sendIntent = new Intent(Intent.ACTION_SEND);
@@ -56,4 +59,60 @@ public class ViewCapturedDataActivity extends AbstractPADListenerActivity {
 				return super.onOptionsItemSelected(item);
 		}
 	}
+
+	protected BaseHelpManager getHelpManager() {
+		return new BaseHelpManager(this, "view_captured_data", 1) {
+
+			@Override
+			public void buildHelpPages(PageBuilder builder) {
+				final ViewPager viewPager = (ViewPager) findViewById(R.id.viewPager);
+				Log.d(getClass().getName(), "buildHelpPages : viewPager = " + viewPager);
+				final int previousSelected = viewPager.getCurrentItem();
+
+				builder.addHelpPage(R.string.view_captured_data_help_captured_data_title, R.string.view_captured_data_help_captured_data_content);
+				builder.addHelpPage(R.string.view_captured_data_help_player_title, R.string.view_captured_data_help_player_content, new ShowcaseHelpPageModel.HelpPageListener() {
+					@Override
+					public void onPreDisplay() {
+						viewPager.setCurrentItem(0);
+					}
+
+					@Override
+					public void onPostDisplay() {
+					}
+				});
+				builder.addHelpPage(R.string.view_captured_data_help_monsters_title, R.string.view_captured_data_help_monsters_content, new ShowcaseHelpPageModel.HelpPageListener() {
+					@Override
+					public void onPreDisplay() {
+						viewPager.setCurrentItem(1);
+					}
+
+					@Override
+					public void onPostDisplay() {
+					}
+				});
+				builder.addHelpPage(R.string.view_captured_data_help_friends_title, R.string.view_captured_data_help_friends_content, new ShowcaseHelpPageModel.HelpPageListener() {
+					@Override
+					public void onPreDisplay() {
+						viewPager.setCurrentItem(2);
+					}
+
+					@Override
+					public void onPostDisplay() {
+						viewPager.setCurrentItem(previousSelected);
+					}
+				});
+				builder.addHelpPage(R.string.view_captured_data_help_share_title, R.string.view_captured_data_help_share_content, R.id.menu_view_captured_data_action_share, null);
+
+			}
+
+			@Override
+			public void buildDeltaHelpPages(PageBuilder builder, int lastDisplayedVersion) {
+				switch (lastDisplayedVersion) {
+					default:
+						buildHelpPages(builder);
+				}
+			}
+		};
+	}
+
 }
