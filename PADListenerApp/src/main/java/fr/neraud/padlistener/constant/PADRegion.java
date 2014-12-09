48d3235e741ae9d3587230ff690f38b57c2b858d
neraud <neraud.login@gmail.com>
2014-12-09 21:14:51 +0100

    Fixed #63 : Added support for Korean PAD

--- PADListenerApp/src/main/java/fr/neraud/padlistener/constant/PADRegion.java
+++ PADListenerApp/src/main/java/fr/neraud/padlistener/constant/PADRegion.java
@@ -6,6 +6,7 @@ public enum PADRegion {
 
 	JP(TimeZone.getTimeZone("Japan")),
 	HK_TW(TimeZone.getTimeZone("Asia/Hong_Kong")),
+	KO(TimeZone.getTimeZone("Asia/Seoul")),
 	US(TimeZone.getTimeZone("America/Los_Angeles"));
 
 	private final TimeZone timeZone;
