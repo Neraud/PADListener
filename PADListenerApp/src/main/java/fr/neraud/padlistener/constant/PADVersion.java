f34321ca991fdbfe9c9e0dff240735939d480350
neraud <neraud.login@gmail.com>
2014-12-18 23:04:09 +0100

    Fixed #69 : Fixed support for the NA Amazon version

--- PADListenerApp/src/main/java/fr/neraud/padlistener/constant/PADVersion.java
+++ PADListenerApp/src/main/java/fr/neraud/padlistener/constant/PADVersion.java
@@ -6,7 +6,7 @@ public enum PADVersion {
 	JP(PADRegion.JP, "api-adr.padsv.gungho.jp", "jp.gungho.pad"),
 	HK_TW(PADRegion.HK_TW, "api-ht-adr.padsv.gungho.jp", "jp.gungho.padHT"),
 	KO(PADRegion.KO, "api-kr-adr.padsv.gungho.jp", "jp.gungho.padKO"),
-	AMAZON_US(PADRegion.US, "api-na-amz-pad.gungho.jp", null);
+	AMAZON_US(PADRegion.US, "api-na-amz.padsv.gungho.jp", "jp.gungho.padKINEN");
 
 	private final PADRegion region;
 	private final String serverHostName;
